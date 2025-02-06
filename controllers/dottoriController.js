220f7f07616a0d22f6fd391abed65d9679f0454e
LorenzoEufemi <lorenzoeufemi@gmail.com>
2025-02-06 15:43:26 +0100

    feat add index

--- controllers/dottoriController.js
+++ controllers/dottoriController.js
@@ -1,4 +1,44 @@
+const dbConnection = require ("../data/dbConnection");
+const slugify = require ("slugify");
+
 const index = (req, res, next) => {
+    const filters = req.query;
+    let sql = `
+    SELECT *
+    FROM dottori
+    `;
+    const params = [];
+    dbConnection.query(sql, params, (err, dottori) => {
+        if (err){
+            return next(new Error(err.message))
+        }
+        return res.status(200).json({
+            status: "success",
+            data: dottori
+        })
+
+    });
+
+
+};
+
+const show = (req, res, next) => {
 
     
+};
+
+const store = (req, res, next) => {
+
+    
+};
+const destroy = (req, res, next) => {
+
+    
+};
+
+module.exports = {
+    index,
+    show,
+    store,
+    destroy
 }
--- routers/dottoriRouter.js
+++ routers/dottoriRouter.js
@@ -1,5 +1,6 @@
 const express = require ("express");
 const router = express.Router();
+const dottoriController = require ("../controllers/dottoriController")
 
 router.get("/", dottoriController.index);
 
