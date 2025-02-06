3b1905acd49cbe57122f1a975e5b925ed65cdb4c
LorenzoEufemi <lorenzoeufemi@gmail.com>
2025-02-06 12:52:17 +0100

    feat: add structure backend

---
+++ app.js
@@ -0,0 +1,8 @@
+const express = require("express");
+
+const app = express();
+const port = process.env.SERVER_PORT;
+
+app.listen(port, () => {
+    console.log(`app is listening on ${port}`);
+});
