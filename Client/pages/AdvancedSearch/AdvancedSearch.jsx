025bdc3c6f66e4d9611f1c5ef197e3b15634346d
FrancescoTullio <francescotullio1992@gmail.com>
2025-02-10 16:27:55 +0100

    feat: globalcontext and search delle specializzazioni

--- Client/pages/AdvancedSearch/AdvancedSearch.jsx
+++ Client/pages/AdvancedSearch/AdvancedSearch.jsx
@@ -1,8 +1,19 @@
+import { useContext, useEffect } from "react";
+import GlobalContext from "../../context/GlobalContext";
+
 function AdvancedSearch() {
+
+    const {ricerca} = useContext(GlobalContext)
+    console.log(ricerca);
+
+    useEffect(() => {
+
+    }, [])
+    
 
     return (
         <>
-            <h1>ciao sono AdvanceSearch</h1>
+            <h1>ciao sono AdvanceSearch {ricerca}</h1>
         </>
     )
 };
