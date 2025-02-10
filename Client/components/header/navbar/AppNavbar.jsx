290e3b968aa133ac937bbf83d76dc99c322d7ba2
lucaconigliaro <lucaconigliaro1@gmail.com>
2025-02-10 15:33:49 +0100

    feat: ADD basic navbar header

--- Client/components/header/navbar/AppNavbar.jsx
+++ Client/components/header/navbar/AppNavbar.jsx
@@ -1,9 +1,40 @@
+import { NavLink } from "react-router-dom";
+import styles from './AppNavbar.module.css'; 
+
 function AppNavbar() {
+
+    const navLinks = [
+        {
+            path: "/",
+            title: "Home"
+        },
+        {
+            path: "/accedi",
+            title: "Accedi"
+        }
+    ];
+
     return (
-        <>
-        <h1>Ciao sono Navbar</h1>
-        </>
-    )
+        <section>
+            <div className={styles.navbar}>
+                <NavLink to="/" className={styles.textLogo}>BDoctors</NavLink>
+                <ul className={styles.navLinks}>
+                    {navLinks.map((curLink, index) => (
+                        <li key={index}>
+                            <NavLink
+                                to={curLink.path}
+                                className={({ isActive }) =>
+                                    isActive ? styles.navLinkActive : styles.navLink
+                                }
+                            >
+                                {curLink.title}
+                            </NavLink>
+                        </li>
+                    ))}
+                </ul>
+            </div>
+        </section>
+    );
 };
 
 export default AppNavbar;
