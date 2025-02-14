702a18660b1c682ae03d4b51915a70dc0fb03d0f
ritaurbani <ritaurbani@gmail.com>
2025-02-14 12:25:36 +0100

    feat: homepage change- dopo review backend

--- Client/components/HomePageCard/HomePageCard.jsx
+++ Client/components/HomePageCard/HomePageCard.jsx
@@ -18,16 +18,21 @@ const HomePageCard = () => {
     return (
         <>
             {doctors.slice(0, 6).map((doc, index) => (
-                <div className="card hp-card border-0 px-3" key={doc.id} style={{maxWidth: "350px"}}>
+                <div className="card hp-card border-0 px-3" key={doc.id} style={{ maxWidth: "350px" }}>
                     <div className="d-flex align-items-center gap-4">
-                        <img src="/doc.jpg" className="rounded-circle" style={{maxWidth: "100px"}} alt="Foto profilo" />
+                        <img src="/doc.jpg" className="rounded-circle" style={{ maxWidth: "100px" }} alt="Foto profilo" />
 
-                            <div className='card-text'>
-                                <h6 className="mb-0 fw-bold">{doc.firstname} {doc.lastname}</h6>
-                                <span className="text-muted mb-1 small">{doc.city}</span>
-                                <p className="text-muted mb-1 small">{doc.specializations}</p>
-                                <Link to={`/doctors/:${doc.slug}`} onClick={() => { setSlugDoctor(doc.slug) }} className="text-primary small fw-semibold">Mostra profilo</Link>
-                            </div>
+                        <div className='card-text'>
+                            <h6 className="mb-0 fw-bold">{doc.firstname} {doc.lastname}</h6>
+                            <span className="text-muted mb-1 small">{doc.city}</span>
+                            {
+                                doc.specializations.map((spec) => (
+                                    <p className="text-muted mb-1 small">{spec.specialization}</p>
+
+                                ))
+                            }
+                            <Link to={`/doctors/:${doc.slug}`} onClick={() => { setSlugDoctor(doc.slug) }} className="text-primary small fw-semibold">Mostra profilo</Link>
+                        </div>
                     </div>
                 </div>
             ))}
