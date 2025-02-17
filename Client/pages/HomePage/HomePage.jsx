1fc8354dc01ec0f9312dff63d57eae1dbb2a139f
LorenzoEufemi <lorenzoeufemi@gmail.com>
2025-02-17 12:44:29 +0100

    add new navbar fix

--- Client/pages/HomePage/HomePage.jsx
+++ Client/pages/HomePage/HomePage.jsx
@@ -20,31 +20,38 @@ function HomePage() {
             <div className="hero-container">
                 <div className="hero-content">
 
-                    <h1 className="text-center" style={{color:" rgba(23, 164, 138, 0.7)"}}>Benvenuto su BDoctors</h1>
+                    <h1 className="text-center testo-lungo" style={{ color: " rgba(23, 164, 138, 0.7)" }}>Benvenuto su BDoctors</h1>
+                    <h1 className="text-center testo-corto" style={{ color: " rgba(23, 164, 138, 0.7)" }}>Benvenuto </h1>
                     {
                         (allSpec === null) ? <div>
                             <p>aspetta</p>
                         </div> :
-                            <div>
-                                <label htmlFor="" className=" text-center" style={{color: "#2B6394"}}>Seleziona il medico per la specializzazione che ti serve</label>
-                                <select onChange={() => handleSelect(event)} className="form-select mb-2" aria-label="Default select example">
-                                    <option value={"null"} name={""}>--</option>
-                                    {
-                                        allSpec.map(curElem => (
-                                     
-                                            <option name={curElem.specialization} value={curElem.id} key={curElem.specialization}>{curElem.specialization}
-                                            </option>
-                                        ))
-                                    }
-                                </select>
-                                <Link to={`/doctors/?specialization=${selectedSpec}`} className="btn text-white"  style={{ backgroundColor: "rgba(23, 164, 138, 0.6)"}}>cerca</Link>
+                            <div className="d-flex align-items-center">
+                                <div>
+                                    <label htmlFor="" className="testo-lungo text-center" style={{ color: "#2B6394" }}>Seleziona il medico per la specializzazione che ti serve</label>
+                                    <label htmlFor="" className="testo-corto text-center" style={{ color: "#2B6394" }}>Seleziona la specializzazione </label>
+                                    <label htmlFor="" className="testo-cortissimo text-center" style={{ color: "#2B6394" }}>Specializzazione</label>
+                                    <select onChange={() => handleSelect(event)} className="form-select mb-2" aria-label="Default select example">
+                                        <option value={"null"} name={""}>--</option>
+                                        {
+                                            allSpec.map(curElem => (
+
+                                                <option name={curElem.specialization} value={curElem.id} key={curElem.specialization}>{curElem.specialization}
+                                                </option>
+                                            ))
+                                        }
+                                    </select>
+                                </div>
+                                <div>
+                                    <Link  to={`/doctors/?specialization=${selectedSpec}`} className="btn text-white mt-3 ms-2" style={{ backgroundColor: "rgba(23, 164, 138, 0.6)" }}>cerca</Link>
+                                </div>
                             </div>
                     }
                 </div>
             </div>
             <div className="spacing" style={{ height: "90px" }}></div>
             <TopDocs />
-            <DiconoDiNoi/>
+            <DiconoDiNoi />
         </>
     );
 };
