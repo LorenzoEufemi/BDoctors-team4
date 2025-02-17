10876260c7a78e18065313505ddab30c541b6312
TommasoVilla91 <tommasovilla@yahoo.it>
2025-02-17 16:42:06 +0100

    debug: doc form

--- Client/pages/Login/Login.jsx
+++ Client/pages/Login/Login.jsx
@@ -1,9 +1,13 @@
+import { Navigate } from "react-router-dom";
 import DocRegForm from "../../components/main/DocRegForm";
 
 function Login() {
     return (
         <>
             <div className="registrazione">
+                <button className="btn-back" onClick={() => Navigate(-1)}>
+                    <i class="fa-solid fa-caret-left" style={{ color: "#4FBE89" }}></i>
+                </button>
                 <h3 className="text-center pt-5 titolo">Non ancora registrato?</h3>
                 <h5 className="text-center sottotitolo">Compila il form</h5>
                 <section className="container">
