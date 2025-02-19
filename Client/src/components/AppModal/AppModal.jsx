3bd6fc905beab48f39f0f421af651b3bffefff20
Luca Conigliaro <lucaconigliaro1@gmail.com>
2025-02-19 12:58:03 +0100

    fix: correct tabIndex attribute in AppModal and update className syntax in DoctorCard

--- Client/src/components/AppModal/AppModal.jsx
+++ Client/src/components/AppModal/AppModal.jsx
@@ -14,7 +14,7 @@ function AppModal({ nome, cognome }) {
             </button>
 
             {/* <!--Modal --> */}
-            <div className="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+            <div className="modal fade" id="exampleModal" tabIndex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
                 <div className="modal-dialog">
                     <div className="modal-content" >
                         <div className="modal-header text-white" style={{ backgroundColor: "#2B6394" }}>
