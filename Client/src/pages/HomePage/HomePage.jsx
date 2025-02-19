90219cb41f6399c81224ca04d16abf2a0b983698
LorenzoEufemi <lorenzoeufemi@gmail.com>
2025-02-19 15:03:51 +0100

    feat : debug url

--- Client/src/pages/HomePage/HomePage.jsx
+++ Client/src/pages/HomePage/HomePage.jsx
@@ -62,7 +62,7 @@ function HomePage() {
                                     </select>
                                 </div>
                                 <div>
-                                    <Link to={`/doctors?${selectedSpec ? "specialization=" + selectedSpec : ""} `} onClick={() => handleSearch(event)} className="btn text-white mt-3 ms-2 home-btn" style={{ backgroundColor: "rgba(23, 164, 138, 0.6)" }}>cerca</Link>
+                                    <Link to={`/doctors/?${selectedSpec ? "specialization=" + selectedSpec : ""} `} onClick={() => handleSearch(event)} className="btn text-white mt-3 ms-2 home-btn" style={{ backgroundColor: "rgba(23, 164, 138, 0.6)" }}>cerca</Link>
                                 </div>
                             </div>
                     }
